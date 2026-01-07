22cb1efc97cec8d89a49e7f6623429daf1792b50
jawwad9 <jawwadmuhammad514@gmail.com>
2026-01-07 03:48:54 -0800

    projects add

--- src/assets/MyProjects.js
+++ src/assets/MyProjects.js
@@ -3,19 +3,55 @@ const projects = [
     name: "React Blogging App",
     description:
       "A full-featured blogging platform built with React, Vite, Tailwind CSS, and Firebase. It allows users to write, edit, and delete posts, with real-time updates and user authentication.",
-    gitLink: "https://github.com/ubaiidraza/React-Blogging-App",
-    liveLink: "https://react-blogging-app-omega.vercel.app/",
+    gitLink: "https://github.com/jawwad9/Blogging-app.git",
+    liveLink: "https://blogging-app-react-js.netlify.app/",
     isLive: true,
     image: "/images/bloging.png",
   },
   {
-    name: "React Password Generator",
+    name: "React Quiz App",
+    description:
+      "A responsive Quiz App built with React.js featuring multiple-choice questions, real-time score tracking, and instant feedback, demonstrating interactive UI and React state management skills.",
+    gitLink: "https://github.com/jawwad9/Quiz-App-with-React.js.git",
+    liveLink: "https://quiz-app-with-react-js.netlify.app/",
+    isLive: true,
+    image: "src/assets/Quiz-App.png",
+  },
+  {
+    name: "React Weather App",
+    description:
+      "React Weather App: Fetches and displays live weather using API integration and React hooks",
+    gitLink: "https://github.com/jawwad9/React-Weather-App.git",
+    liveLink: "https://eloquent-phoenix-4ba128.netlify.app/",
+    isLive: true,
+    image: "src/assets/Weather-App.png",
+  },
+  {
+    name: "JavaScript Recipe App",
+    description:
+      "🧾 Simple and responsive recipe search app built with HTML, CSS & JavaScript using API integration to fetch and display recipes with ingredients and popup details.",
+    gitLink: "https://github.com/jawwad9/Recipe-App-with-javaScript.git",
+    liveLink: "https://velvety-capybara-5efc0c.netlify.app/",
+    isLive: true,
+    image: "src/assets/Recipe-App.png",
+  },
+  {
+    name: "JavaScript Ecommerce App",
+    description:
+      "🛍️ Fake Store Web App — A simple Ecommerce website built with HTML, CSS, and JavaScript featuring product cards, add-to-cart system, login/register authentication, and responsive UI with modal checkout.",
+    gitLink: "https://github.com/jawwad9/Ecommerce-App-With-JavaScript.git",
+    liveLink: "https://ecommerce-app-with-javascript.netlify.app/",
+    isLive: true,
+    image: "src/assets/E-Store.png",
+  },
+  {
+    name: "JavaScript Simple CRUD (LocalStorage)",
     description:
       "🧾 Simple CRUD App using JavaScript (LocalStorage) A basic Create, Read, Update, and Delete (CRUD) web app built with pure JavaScript. It stores data in browser localStorage with form validation for name and email fields.",
     gitLink: "https://github.com/jawwad9/JavaScript-CRUD-and-Local-Storage.git",
-    liveLink: "https://javascript-curd.netlify.app/",
+    liveLink: "hhttps://javascript-curd.netlify.app/",
     isLive: true,
-    image: "src/assets/01.png",
+    image: "src/assets/Todo-App.js.png",
   }
 ];
 
